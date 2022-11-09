7354c7ea206c9a148ee26915f83b169254dd9c64
mohamed <mastrocoding@gmail.com>
2022-11-10 00:25:47 +0200

    change the path of image output

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,6 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        use: "file-loader?name=[name].[ext]",
         type: "asset/resource",
       },
       {
